efb8794fda2b0135567d91cdfc6948f8b88fd743
LordRonz <LordRonz@users.noreply.github.com>
2022-02-14 14:43:13 +0000

    chore: dont run gute nacht today :ok_hand:

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from typing import Optional
 
 USERNAME: Optional[str] = "aaron v2.0"
 AVATAR_URL: Optional[str] = "https://cdn.discordapp.com/avatars/526630470566281226/bebc10011c9849f56cb4c47ec82f0cb9.webp?size=2048"
-EXECUTE_TODAY: bool = True
+EXECUTE_TODAY: bool = False
