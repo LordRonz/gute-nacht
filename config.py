1f3e7c5fde86921328170fba3450e7c069417207
LordRonz <aarontanhar2000@gmail.com>
2022-02-14 19:39:31 +0700

    fix: python 3.9 compatibility :rocket:

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-USERNAME: str | None = "aaron v2.0"
-AVATAR_URL: str | None = "https://cdn.discordapp.com/avatars/526630470566281226/bebc10011c9849f56cb4c47ec82f0cb9.webp?size=2048"
+from typing import Optional
+
+USERNAME: Optional[str] = "aaron v2.0"
+AVATAR_URL: Optional[str] = "https://cdn.discordapp.com/avatars/526630470566281226/bebc10011c9849f56cb4c47ec82f0cb9.webp?size=2048"
 EXECUTE_TODAY: bool = True
