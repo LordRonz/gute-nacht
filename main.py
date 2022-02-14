20f95526836c5a1efb06828ebccc8d3a06cb7c0f
LordRonz <aarontanhar2000@gmail.com>
2022-02-14 19:34:18 +0700

    ci: add github actions ci :rocket:

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from discord_webhook.discord_webhook import DiscordWebhook
 def main():
     read_env()
     is_ci = os.getenv("CI", "") == "true"
-    WEBHOOK_URL: str = os.getenv("WEBHOOK_URL_DEV" if is_ci else "WEBHOOK_URL", "")
+    webhook_url: str = os.getenv("WEBHOOK_URL_DEV" if is_ci else "WEBHOOK_URL", "")
+    if is_ci and not webhook_url:
+        webhook_url = os.getenv("WEBHOOK_URL", "")
 
     quote = choice(quotes)
 
@@ -19,25 +21,13 @@ def main():
     content = f"**{quote}** :sleeping: :full_moon_with_face:\n{gif}"
 
     webhook = DiscordWebhook(
-        url=WEBHOOK_URL,
+        url=webhook_url,
         content=content,
         username=config.USERNAME,
         avatar_url=config.AVATAR_URL,
     )
 
     webhook.execute()
-
-    # payload = {
-    #     "username": config.USERNAME,
-    #     "avatar_url": config.AVATAR_URL,
-    #     "content": f"**{quote}** :sleeping: :full_moon_with_face:\n{gif}",
-    # }
-
-    # logger.info(payload)
-
-    # res = requests.post(WEBHOOK_URL, json=payload)
-
-    # logger.info(f"http response status: {res.status_code}")
 
 
 if __name__ == "__main__":
