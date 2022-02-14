968a8e749fcef03863fa6e70e08beecb20e6fda7
LordRonz <aarontanhar2000@gmail.com>
2022-02-14 17:18:49 +0700

    feat: use add toggle execute script :rocket:

---
+++ toggle_execute.py
@@ -0,0 +1,47 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Toggle execute gute nacht")
+    parser.add_argument(
+        "-n", "--no-execute", action="store_true", help="Don't execute gute nacht today"
+    )
+    parser.add_argument(
+        "-e", "--execute", action="store_true", help="Execute gute nacht today"
+    )
+
+    return parser.parse_args().__dict__
+
+
+EXECUTE_TRUE = "EXECUTE_TODAY: bool = True\n"
+EXECUTE_FALSE = "EXECUTE_TODAY: bool = False\n"
+
+
+def toggle(yes=True, neutral=True):
+    with open("./config.py", "r+") as conf:
+        conf_lines = list(conf)
+
+    conf_lines[-1] = (
+        (EXECUTE_FALSE if conf_lines[-1] == EXECUTE_TRUE else EXECUTE_TRUE)
+        if neutral
+        else (EXECUTE_TRUE if yes else EXECUTE_FALSE)
+    )
+
+    with open("./config.py", "w") as conf:
+        conf.writelines(conf_lines)
+
+
+def main():
+    args = get_args()
+    ex = args["execute"]
+    no_ex = args["no_execute"]
+    neutral = not ex and not no_ex
+    if ex and no_ex:
+        print("Invalid options!")
+        return
+    yes = ex
+    toggle(yes=yes, neutral=neutral)
+
+
+if __name__ == "__main__":
+    main()
