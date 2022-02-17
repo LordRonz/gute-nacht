1ce2e2127301bdd496beb60e77b32b41501e5992
LordRonz <aarontanhar2000@gmail.com>
2022-02-17 15:02:25 +0700

    feat: add thumbnails, custom emojis, and embed urls :rocket:

---
+++ data/embed_urls.py
@@ -0,0 +1,12 @@
+embed_urls = [
+    "https://lordronz.github.io/paste/#XQAAAQAdAAAAAAAAAAAjm+zIIDcadIruKEx6MvtkwRwWMHJkqX4RDBm1Ashxwv//F6YAAA==",
+    "https://lordronz.github.io/paste/#XQAAAQAaAAAAAAAAAAAjm+zIbjWDejv4784sCrEr6yumcLQhrSeMEPWd8x3/4W4gAA==",
+    "https://lordronz.github.io/paste/#XQAAAQAMAAAAAAAAAAApmwi4cBAZBj2xB5SdtGDS//6liAA=",
+    "https://lordronz.github.io/paste/#XQAAAQAOAAAAAAAAAAAqm4qGq03WYPnv+7+32Ir2uuz//WegAA==",
+    "https://lordronz.github.io/paste/#XQAAAQAeAAAAAAAAAAAhmwnnZotGWl5ibs6FvX4w9iymKtzuWJ7RdsGrT9LXh15I//9DGAAA",
+    "https://lordronz.github.io/paste/#XQAAAQAdAAAAAAAAAAApmwi4cBAZxX454LGfANc3ROYgt7R7yHmTNXw1pSvcQB//3NdAAA==",
+    "https://lordronz.github.io/paste/#XQAAAQAZAAAAAAAAAAAnGkjmqBUeN8+VwqGfRU42IEgQfkwOS///+u9AAA==",
+    "https://lordronz.github.io/paste/#XQAAAQAyAAAAAAAAAAAmmEsiAz3htWjjM6S9eXn4/8ykYoxqW81qgFLoKz439B64ATStMzLbYd4Ot4+rF2Ej/+/IMAA=",
+    "https://lordronz.github.io/paste/#XQAAAQAZAAAAAAAAAAAkGErGZJKY/XOsOUiYlvlAV95m4/UhWZXpse5ez//+9pwA",
+    "https://lordronz.github.io/paste/#XQAAAQAYAAAAAAAAAAAnGkqGZJLVrxA3/FfGsFQa64mphgjqQu1+e///RgQAAA==",
+]
---
+++ data/thumbnails.py
@@ -0,0 +1,6 @@
+thumbnails = [
+    "https://c.tenor.com/0PP9WiddZWcAAAAC/anime-sleepy.gif",
+    "https://thumbs.gfycat.com/FearlessAnyAlaskanmalamute-size_restricted.gif",
+    "https://c.tenor.com/Bx_eD4jd8jQAAAAC/anime-sleepy.gif",
+    "https://c.tenor.com/re9a71mA5xwAAAAC/nogamenolife-shiro.gif",
+]
